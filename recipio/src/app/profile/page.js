253c055ea7cd199c809ba3b45666062acf68e35f
Manuel <pedraza.sanchez.manuel.alejandro@gmail.com>
2025-01-03 12:53:34 -0500

    Created base project

---
+++ recipio/src/app/profile/page.js
@@ -0,0 +1,8 @@
+
+export default function Profile() {
+    return (
+        <div >
+            Hello there from profile
+        </div>
+    );
+}
---
+++ recipio/src/app/recipies/page.js
@@ -0,0 +1,8 @@
+
+export default function Recipies() {
+    return (
+        <div >
+            Hello there from recipies
+        </div>
+    );
+}
